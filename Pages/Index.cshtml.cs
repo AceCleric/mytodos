97ddfca424889230ac8680ea45cfb8eaeff30689
AceCleric <anouar_azzouz@outlook.com>
2023-06-07 22:36:57 +0200

    Display json data in the view

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using mytodos.Models;
+using mytodos.Services;
 
 namespace mytodos.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    public JsonFileTodoService TodoService;
+    public IEnumerable<Todo> Todos { get; private set; }
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, JsonFileTodoService todoService)
     {
         _logger = logger;
+        TodoService = todoService;
     }
 
     public void OnGet()
     {
-
+        Todos = TodoService.GetTodos();
     }
 }
